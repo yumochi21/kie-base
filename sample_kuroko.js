7003bd3dd8a481f07b8d54d0159ab9bb4437b624
望月由登 <mochidzukiyutou@mochidzukiyutounoMacBook-puro.local>
2018-12-01 03:21:15 +0900

    kuroko module is modified

--- sample_kuroko.js
+++ sample_kuroko.js
@@ -3,7 +3,3 @@ var kuroko = require('./utils/kuroko')
 kuroko.analyse('川崎市の人口は何人ですか？').then(function(result) {
     console.log(result)
 })
-
-// kuroko.analyse('川崎市の人口は何人？').then(function(result) {
-//     console.log(result)
-// })
